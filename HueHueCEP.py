3fd1e36051937ba852da276538b08adf0ce17f42
Hiagor Queiros de Oliveira <hiagornew@gmail.com>
2018-10-02 22:23:03 -0300

    change function json_complete for result_address

--- HueHueCEP.py
+++ HueHueCEP.py
@@ -7,7 +7,7 @@ class HueHueCEP:
         self.cep = cep.replace('-','') 
         
         
-    def json_completo(self):
+    def result_address(self):
         resp = requests.get("http://cep.republicavirtual.com.br/web_cep.php?cep=%s&formato=json" % self.cep)
         data = json.loads(resp.text)
         return data
